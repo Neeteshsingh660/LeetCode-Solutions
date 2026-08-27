050ff0fa5f950fd237a9aca6ee4a1f588aaf6eec
Neeteshsingh660 <neeteshsingh660@gmail.com>
2026-08-28 04:43:21 +0530

    Time: 12 ms (5.19%), Space: 47.2 MB (59%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,47 +1,89 @@
+// class Solution {
+//     public int findCircleNum(int[][] isConnected) {
+//      ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+//         for(int i=0;i<isConnected.length;i++)
+//         {
+//             adj.add(new ArrayList<>());
+//         }
+//         for(int i=0;i<isConnected.length;i++)
+//         {
+//             for(int j=0;j<isConnected.length;j++)
+//             {
+//                 if(i!=j&&isConnected[i][j]==1)
+//                 {
+//                     adj.get(i).add(j);
+//                 }
+//             }
+//         }
+
+//         System.out.println(adj);
+//         boolean vis[]=new boolean[isConnected.length];
+//         int c=0;
+//         for(int i=0;i<vis.length;i++)
+//         {
+//              if(!vis[i])
+//              {
+//                 dfs(vis,i,adj);
+//                 c++;
+
+//              }
+
+//         }
+//         return c;
+//     }
+
+//     public void dfs(boolean vis[],int i,ArrayList<ArrayList<Integer>> adj)
+//     {
+//         vis[i]=true;
+//         for(int node:adj.get(i))
+//         {
+//             if(!vis[node])
+//             {
+//                 dfs(vis,node,adj);
+
+//             }
+//         }
+//     }
+// }
+
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-     ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<isConnected.length;i++)
-        {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < isConnected.length; i++) {
             adj.add(new ArrayList<>());
         }
-        for(int i=0;i<isConnected.length;i++)
-        {
-            for(int j=0;j<isConnected.length;j++)
-            {
-                if(i!=j&&isConnected[i][j]==1)
-                {
+        for (int i = 0; i < isConnected.length; i++) {
+            for (int j = 0; j < isConnected.length; j++) {
+                if (i != j && isConnected[i][j] == 1) {
                     adj.get(i).add(j);
                 }
             }
         }
-        
-        System.out.println(adj);
-        boolean vis[]=new boolean[isConnected.length];
-        int c=0;
-        for(int i=0;i<vis.length;i++)
-        {
-             if(!vis[i])
-             {
-                dfs(vis,i,adj);
+        Queue<Integer> q = new LinkedList<>();
+        boolean vis[] = new boolean[isConnected.length];
+
+        int c = 0;
+        for (int i = 0; i < vis.length; i++) {
+            if (!vis[i]) {
                 c++;
-                
-             }
-            
+                q.add(i);
+                while (!q.isEmpty()) {
+                    int n = q.poll();
+                    for (Integer node : adj.get(n)) {
+                        if (!vis[node]) {
+                            q.add(node);
+                            vis[node] = true;
+                        }
+
+                    }
+                }
+            }
+          
         }
+        System.out.println(adj);
+
         return c;
+
     }
 
-    public void dfs(boolean vis[],int i,ArrayList<ArrayList<Integer>> adj)
-    {
-        vis[i]=true;
-        for(int node:adj.get(i))
-        {
-            if(!vis[node])
-            {
-                dfs(vis,node,adj);
-            
-            }
-        }
-    }
 }
